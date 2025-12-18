aa1e19bfcf321483e2833185d548a9eeb3782e07
Carlos Yáñez <carlosyanez997@gmail.com>
2025-12-18 17:39:17 +0100

    Update entity.py
    Update all classes

--- bills/entity.py
+++ bills/entity.py
@@ -1,16 +1,23 @@
 from abc import ABC, abstractmethod
+
 class Person(ABC):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dni, email, mobile, address) -> None:
+        self.dni = dni
+        self.email = email
+        self.mobile = mobile
+        self.address = address
     
     @abstractmethod
-    def print(self):
+    def print(self) -> None:
         pass
 
 class Buyer(Person):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dni, full_name, age, email, mobile, address) -> None:
+        super().__init__(dni, email, mobile, address)
+        self.full_name = full_name
+        self.age = age
 
 class Seller(Person):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dni, email, mobile, bussines_name, bussines_addres) -> None:
+        super().__init__(dni, email, mobile, bussines_addres)
+        self.bussines_name = bussines_name
